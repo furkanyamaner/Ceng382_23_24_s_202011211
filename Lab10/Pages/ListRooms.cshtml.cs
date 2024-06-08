de6d78350db6a20cae36bc243d2ace7e41921a15
furkanyamaner <furkanyamanerofficial@gmail.com>
2024-06-09 01:02:28 +0300

    Lab10 yapıldı

---
+++ Lab10/Pages/ListRooms.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore; 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyApp.Namespace
+{
+    public class ListRoomsModel : PageModel
+    {
+        public List<Room> listrooms = new List<Room>();
+        public void OnGet(){
+            try{
+
+            
+            string connectionString ="Server=.\\SQLEXPRESS;Database=WebAppDataBase;Trusted_Connection=True;TrustServerCertificate=True";
+            using(SqlConnection connection = new SqlConnection(connectionString)){
+                connection.Open();
+                String sql ="SELECT * FROM rooms";
+                using(SqlCommand command = new SqlCommand(sql, connection)){
+                    using(SqlDataReader reader = command.ExecuteReader()){
+                        while(reader.Read()){
+                            Room rooms = new Room();
+                            rooms.Id = reader.GetInt32(0);
+                            rooms.RoomName= reader.GetString(1);
+                           
+                            
+                            rooms.Capacity=reader.GetInt32(2);
+                            listrooms.Add(rooms);
+                        }
+                    }
+
+                }
+            }
+            }
+            catch(Exception ex){
+                Console.WriteLine("EXCEPTİON"+ex.ToString());
+            }
+        }
+    }
+}
